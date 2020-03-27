d2f914a1fdb41c22acdee6742c72d62b251023a7
frunox <60527588+frunox@users.noreply.github.com>
2020-03-27 03:33:47 -0500

    basic needs met

--- README.md
+++ README.md
@@ -1,26 +1,29 @@
-# New Project
+# _What a Project_
 
 ## Project Description
 
-## Table of Contents
+IT needs this
 
 ## Table of Contents
+
 
 ## Installation
+DOn't
 
 ## Usage
+It's used code
 
-## License
+## Licensing
+MIT
 
 ## Contributing
+Many
 
 ## Tests
+Testing, testing
 
 ## Questions
+Lots
 
-![](https://img.shields.io/badge/Project-Creator-brightgreen)  frunox  null
+![](https://img.shields.io/badge/Project-Creator-brightgreen) frunox  null
 ![](https://avatars0.githubusercontent.com/u/60527588?v=4)
-
-The goal was to create a well organized readMe.md file.  I managed to get it to be well organized, but was unable to get the descriptive text to enter  under the headings.  I couldn't figure out how to manage the flow of the code.  It kept running regardless of how I tried to stop it.  
-
-I have an idea of how to make it work, but don't have time to implement it before the homework is due.  I'm going to create a series of functions to deal with each heading.
--- index.js
+++ index.js
@@ -11,23 +11,57 @@ const questions = [
   {
     type: "prompt",
     name: "username",
-    message: "Enter your GitHub username"
+    message: "Enter your GitHub username:  "
   },
   {
     type: "prompt",
     name: "projectName",
-    message: "Enter your GitHub project name"
+    message: "Enter your GitHub project name:  "
+  },
+  {
+    type: "input",
+    name: "descriptionText",
+    message: "Enter a project description:  "
+  },
+  {
+    type: "input",
+    name: "tocText",
+    message: "Enter contents information:  "
+  },
+  {
+    type: "input",
+    name: "installText",
+    message: "Enter installation information:  "
+  },
+  {
+    type: "input",
+    name: "usageText",
+    message: "Enter usage information:  "
+  },
+  {
+    type: "input",
+    name: "licenseText",
+    message: "Enter license information:  "
+  },
+  {
+    type: "input",
+    name: "contributorText",
+    message: "Enter contributors:  "
+  },
+  {
+    type: "input",
+    name: "testText",
+    message: "Enter a test description:  "
+  },
+  {
+    type: "input",
+    name: "questionText",
+    message: "Enter questions:  "
   }
 ];
 
-// code to check if question and response code works
-// inquirer.prompt(questions).then(answers => {
-//   console.log("\nCheck answers:");
-//   console.log(JSON.stringify(answers, null, "  "));
-// });
 
-// start API call
-inquirer.prompt(questions).then(async function ({ username, projectName }) {
+inquirer.prompt(questions).then(async function ({ username, projectName, descriptionText, tocText, installText, usageText, licenseText, contributorText, testText, questionText }) {
   // create variable holding the URL for the API containing the ${ username }
   const queryURL = `https://api.github.com/users/${username}`;
 
@@ -38,107 +72,67 @@ inquirer.prompt(questions).then(async function ({ username, projectName }) {
       // console.log(res);
       avatar = res.data.avatar_url;
       email = res.data.email;
-    });
-  // console.log(username);
-  // console.log(projectName);
-  await appendFileAsync("README.md", "# " + projectName + `\n` + "\n")
-    .then(function () {
-      console.log("append Project Name");
-      return
+      console.log(avatar)
     })
-  // .catch(function (err) {
-  //   console.log(err);
-  // });
-  await appendFileAsync("README.md", "## Project Description" + `\n` + `\n`)
     .then(async function () {
-      console.log("append project description header");
-      // enterText();
-      inquirer
-        .prompt([
-          {
-            type: "confirm",
-            message: "Do you want to enter a project description now?",
-            name: "choice"
-          }
-        ])
-        .then(async function ({ choice }) {
-          console.log(`${choice}`)
-          if (choice) {
-            console.log(`Choice is true`)
-            inquirer
-              .prompt([
-                {
-                  type: "input",
-                  message: "Enter the project description",
-                  name: "projectDesc"
-                }
-              ])
-              .then(async function ({ projectDesc }) {
-                console.log("enter description:  " + projectDesc)
-                await appendFileAsync("README.md", projectDesc + `\n`);
-              })
-          } else {
-            console.log("No description text added");
-          }
-        });
-      return
+      await appendFileAsync("README.md", "# _" + projectName + "_" + `\n` + "\n")
     })
-  await appendFileAsync("README.md", "## Table of Contents" + `\n` + `\n`)
-    .then(function () {
-      console.log("append TOC header");
-    })
-  await appendFileAsync("README.md", "## Table of Contents" + `\n` + `\n`)
-    .then(function () {
-      console.log("append TOC header");
-    })
-  await appendFileAsync("README.md", "## Installation" + `\n` + `\n`)
-    .then(function () {
-      console.log("append Installation header");
-    })
-  await appendFileAsync("README.md", "## Usage" + `\n` + `\n`)
-    .then(function () {
-      console.log("append Usage header");
-    })
-  await appendFileAsync("README.md", "## License" + `\n` + `\n`)
-    .then(function () {
-      console.log("append License header");
-    })
-  await appendFileAsync("README.md", "## Contributing" + `\n` + `\n`)
-    .then(function () {
-      console.log("append Contributing header");
-    })
-  await appendFileAsync("README.md", "## Tests" + `\n` + `\n`)
-    .then(function () {
-      console.log("append Tests header");
-    })
-  await appendFileAsync("README.md", "## Questions" + `\n` + `\n`)
-    .then(function () {
-      console.log("append Questions header");
-    })
-  await appendFileAsync('README.md', `![](https://img.shields.io/badge/Project-Creator-brightgreen)  ${username}  ${email}` + `\n`)
     .then(async function () {
-      console.log("append image");
+      await appendFileAsync("README.md", "## Project Description" + `\n` + `\n`)
     })
-  await appendFileAsync('README.md', `![](${avatar})`)
     .then(async function () {
-      console.log("append image");
+      await appendFileAsync("README.md", descriptionText + `\n` + `\n`);
+    })
+    .then(async function () {
+      await appendFileAsync("README.md", "## Table of Contents" + `\n`);
+    })
+    .then(async function () {
+      await appendFileAsync("README.md", tocText + `\n` + `\n`);
+    })
+    .then(async function () {
+      await appendFileAsync("README.md", "## Installation" + `\n`);
+    })
+    .then(async function () {
+      await appendFileAsync("README.md", installText + `\n` + `\n`);
+    })
+    .then(async function () {
+      await appendFileAsync("README.md", "## Usage" + `\n`);
+    })
+    .then(async function () {
+      await appendFileAsync("README.md", usageText + `\n` + `\n`);
+    })
+    .then(async function () {
+      await appendFileAsync("README.md", "## Licensing" + `\n`);
+    })
+    .then(async function () {
+      await appendFileAsync("README.md", licenseText + `\n` + `\n`);
+    })
+    .then(async function () {
+      await appendFileAsync("README.md", "## Contributing" + `\n`);
+    })
+    .then(async function () {
+      await appendFileAsync("README.md", contributorText + `\n` + `\n`);
+    })
+    .then(async function () {
+      await appendFileAsync("README.md", "## Tests" + `\n`);
+    })
+    .then(async function () {
+      await appendFileAsync("README.md", testText + `\n` + `\n`);
+    })
+    .then(async function () {
+      await appendFileAsync("README.md", "## Questions" + `\n`);
+    })
+    .then(async function () {
+      await appendFileAsync("README.md", questionText + `\n` + `\n`);
+    })
+    .then(async function () {
+      await appendFileAsync('README.md', `![](https://img.shields.io/badge/Project-Creator-brightgreen) ${username}  ${email}` + `\n`)
+    })
+    .then(async function () {
+      await appendFileAsync('README.md', `![](${avatar})`)
     })
     .catch(function (err) {
       console.log(err);
     });
 });
 
-async function enterText() {
-  inquirer
-    .prompt([
-      {
-        type: "input",
-        message: "Enter text here (return to skip)",
-        name: "text"
-      }
-    ])
-    .then(async function ({ text }) {
-      console.log("text entered")
-      await appendFileAsync("README.md", text + `\n`);
-    })
-}
